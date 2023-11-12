309b68dd5f69d3efbf76b8822abaa9707ea4c34a
yotu <114195789+ulxsth@users.noreply.github.com>
2023-11-12 19:17:53 +0900

    did!

--- script/script.js
+++ script/script.js
@@ -16,11 +16,28 @@ camera.position.z = 1;
 // レンダラーを作成
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0xbaf9ff, 1);
+renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
+
+// 上部に光源を追加
+const lightTop = new THREE.DirectionalLight(0xffffff, 10);
+lightTop.position.set(10, 1, -2);
+lightTop.castShadow = true;
+scene.add(lightTop);
 
 // 自然光を追加
 const light = new THREE.AmbientLight(0xffffff, 1);
 scene.add(light);
+
+// 床を追加
+const floorGeometry = new THREE.PlaneGeometry(10, 10);
+const floorMaterial = new THREE.MeshBasicMaterial({ color: 0xa3e9f0 });
+const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+floor.rotation.x = degreesToRadians(-90);
+floor.position.y = -0.5;
+floor.receiveShadow = true;
+scene.add(floor);
 
 // 3Dモデルを読み込む
 const loader = new GLTFLoader();
@@ -28,6 +45,11 @@ loader.load('../assets/gltf/model.gltf', function (gltf) {
   gltf.scene.scale.set(0.3, 0.3, 0.3);
   gltf.scene.rotation.y = degreesToRadians(150);
   gltf.scene.position.y = -0.5;
+  gltf.scene.traverse(function (node) {
+    if (node.isMesh) {
+      node.castShadow = true;
+    }
+  });
   scene.add(gltf.scene);
 });
 
